1cf4144c264713d5ec1bd71314e0b9b39463ebe5
Dm Ant <Dm@Ant.ru>
2024-02-15 17:22:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/profi24/Splash.java
@@ -0,0 +1,43 @@
+package com.example.profi24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_splash2);
+                new CountDownTimer(3000,1000){
+                    @Override
+                    public void onTick(long l) {
+
+                    }
+                    @Override
+                    public void onFinish() {
+                        startActivity(new Intent(Splash.this, OnBoarding.class));
+                        finish();
+                    }
+                }.start();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
+
+
+
+
+    }
+}
